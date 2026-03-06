63423c6ad9498daeaec53ceb748dd1f4ff4cd999
blackdui <2501862946@qq.com>
2026-03-06 15:56:49 +0800

    added some stuff.

---
+++ exercise2/src/data_structure/linked_list.cpp
@@ -0,0 +1,49 @@
+//
+// Created by blackdui on 2026/3/4.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+//静态链表
+const int kNodeNum = 100;
+struct Node {
+  int id;
+  int data;
+  int next_id;
+} nodes[kNodeNum];
+
+void Delete(Node nodes[], int id) {
+  //pre不一定是上一个元素
+  nodes[id - 1].next_id = id + 1;
+}
+void Initialize(Node nodes[], int len) {
+  nodes[0].next_id = 1;
+  for (int i = 1; i < len - 1; i++) {
+    nodes[i].id = i;
+    nodes[i].next_id = i + 1;
+  }
+  //循环链表
+  nodes[len - 1].next_id = 1;
+}
+void Show(Node nodes[], int len) {
+  for (int i = 0; i < len - 1; i++) {
+    int now = nodes[0].next_id;
+    cout << nodes[now].data << " ";
+    now = nodes[now].next_id;
+  }
+  cout << endl;
+}
+int main() {
+  // 初始化
+
+  Initialize(nodes, 10);
+  //遍历链表
+
+  //删除节点
+  
+  Delete(nodes, 8); 
+  //插入节点
+  Insert();
+  return 0;
+}
---
+++ exercise2/src/stl/string_examp.cpp
@@ -0,0 +1,3 @@
+//
+// Created by blackdui on 2026/3/3.
+//
